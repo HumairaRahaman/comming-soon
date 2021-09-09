76111a07cd3b6dffa7263ed44e0629f25850fc5f
HumairaRahaman <humairamitul@gmail.com>
2021-09-09 23:41:31 +0600

    final work

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'pamiry' : ['Open Sans', 'sans-serif'],
+      }
+    },
   },
   variants: {
     extend: {},
